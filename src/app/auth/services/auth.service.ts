d1ade49872356d3e4a1e3e85326922503acb6f61
Lucas Ciapparelli <lucasciappa97@gmail.com>
2023-06-05 19:15:22 -0300

    add: local services

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environments';
 import { AuthStatus, User } from '../interfaces';
@@ -15,12 +15,35 @@ export class AuthService {
   private _currentUser = signal<User|null>(null);
   private _authStatus = signal<AuthStatus>( AuthStatus.checking );
 
+  //! Al mundo exterior
+  public currentUser = computed( () => this._currentUser() );
+  public authStatus = computed( () => this._authStatus() );
+
   constructor() { }
+
+  private setAuthentication(user: User): boolean {
+
+    this._currentUser.set( user );
+    this._authStatus.set( AuthStatus.authenticated );
+    localStorage.setItem('user', JSON.stringify(user));
+
+    return true;
+  }
 
   login(email: string, password: string): Observable<boolean>{
     
+    const url  = `${ this.baseUrl }/auth/login`;
+    const body = { email, password };
+
+    this.setAuthentication( body )
+
     return of(true);
 
+  }
+
+  logout() {
+    this._currentUser.set(null);
+    this._authStatus.set( AuthStatus.notAuthenticated );
   }
 
 }
