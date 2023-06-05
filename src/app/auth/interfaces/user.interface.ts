ce67bd48f82d1548f377f04d652c2773c3bf5d6a
Lucas Ciapparelli <lucasciappa97@gmail.com>
2023-06-05 18:18:56 -0300

    add: auth interfaces

---
+++ src/app/auth/interfaces/user.interface.ts
@@ -0,0 +1,12 @@
+
+
+export interface User {
+    _id:      string;
+    dni:      string;
+    email:    string;
+    lastName: string;
+    name:     string;
+    phone:    string;
+    isActive: boolean;
+    roles:    string[];
+  }
