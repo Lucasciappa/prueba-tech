9a2c31ca2a4bf92b05b1348588678c05e1fed4c1
Lucas Ciapparelli <lucasciappa97@gmail.com>
2023-06-05 18:15:18 -0300

    add: login form

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    email:    ['lucasciappa97@gmail.com', [ Validators.required, Validators.email ]],
+    password: ['123456', [ Validators.required, Validators.minLength(6) ]],
+  });
+
+  login() {
+    const { email, password } = this.myForm.value;
+
+    console.log(email, password);
+    
+    // this.authService.login(email, password)
+    //   .subscribe({
+    //     next: () => this.router.navigateByUrl('/dashboard'),
+    //     error: (message) => {
+    //       Swal.fire('Error', message, 'error' )
+    //     }
+    //   })
+
+  }
 
 }
