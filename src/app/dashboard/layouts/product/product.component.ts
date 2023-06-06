f0303b98a3444416dd768df1515ccf452fa34846
Lucas Ciapparelli <lucasciappa97@gmail.com>
2023-06-06 15:49:45 -0300

    fix: css files to scss

--- src/app/dashboard/layouts/product/product.component.ts
+++ src/app/dashboard/layouts/product/product.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ProductsService } from '../../service/products.service';
 import { CartService } from '../../service/cart.service';
 import { SubcategoryService } from '../../service/sub-categories.service';
-
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -11,20 +10,19 @@ import { SubcategoryService } from '../../service/sub-categories.service';
 export class ProductComponent {
   productos: any[] = [];
   subcategorias: any[] = [];
+  page!: number;
 
   constructor(
     private productsService: ProductsService,
     private cartService: CartService,
     private subcategoriesService: SubcategoryService
     ) {}
-
     getProductList(): void {
       this.productsService.getProductos().subscribe((data: any) => {
         this.productos = data;
         this.productsService.getImageUrl(this.productos);
       });
     }
-
     addToCart(product: any): void {
       this.cartService.addToCart(product);
     }
@@ -35,16 +33,13 @@ export class ProductComponent {
       this.productos = data;
       this.productsService.getImageUrl(this.productos);
     }
-
-
+    
+    
     const subcategories = await this.subcategoriesService.getSubcategories().toPromise();
     if (subcategories) {
       console.error(subcategories);
       this.subcategorias = subcategories;
     }
-
-
-
 
   }
 }
